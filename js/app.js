5e0a5c3ac4444b4635cd7d80c2b88812cbd2bd78
Juan <lrccfj@outlook.com>
2021-07-05 08:54:34 +0100

    feat: Add scrollIntoView, add arrow functions and refactor, full basic requirements.

--- js/app.js
+++ js/app.js
@@ -2,38 +2,51 @@
 const navElements = [];
 const sections = document.querySelectorAll('section[id^="section"]');
 
-
 // Build menu 
-for (let i = 0; i < sections.length; i++) {
-    navElements.push(sections[i].dataset.nav);
+sections.forEach((section) => navElements.push(section.dataset.nav));
+
+const buildMenu = () => {
+    for (element of navElements) {
+        let listItem = document.createElement('li');
+        listItem.className = "navbar__item";
+        listItem.innerHTML = `<a class="anchor_menu">${element}</a>`;
+        document.querySelector('#navbar__list').appendChild(listItem);
+    }
 }
 
-for (element of navElements) {
-    let listItem = document.createElement('li');
-    listItem.className = "navbar__item";
-    // Scroll to section on link click
-    listItem.innerHTML = `<a href="#${element.toLowerCase().replace(" ", "")}">${element}</a>`;
-    document.querySelector('#navbar__list').appendChild(listItem)
+//Add eventlistener to Menu and scroll to section
+const scrollToSection = () => {
+    document.querySelectorAll('a[class="anchor_menu"]').forEach((element) => {
+        console.log(element);
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            sections.forEach(section => {
+                if (section.dataset.nav === element.innerText) {
+                    section.scrollIntoView();
+                }
+            })
+        })
+    });
 }
-
 
 // Set sections as active
-
-function updateScrolling() {
-
-    for (let i = 0; i < sections.length; i++) {
-        console.log(sections[i].getBoundingClientRect());
-        sections[i].getBoundingClientRect();
-        if (sections[i].getBoundingClientRect().top >= -170 && sections[i].getBoundingClientRect().top <= 390) {
+const activeStateScrolling = () => {
+    sections.forEach(section => {
+        section.getBoundingClientRect();
+        if (section.getBoundingClientRect().top >= -170 && section.getBoundingClientRect().top <= 390) {
             // Add class 'active' to section when near top of viewport
-            sections[i].classList.add('your-active-class');
+            section.classList.add('your-active-class');
         } else {
-            sections[i].classList.remove('your-active-class');
+            section.classList.remove('your-active-class');
         }
-    }
-
+    })
 }
 
-document.addEventListener('scroll', updateScrolling);
+
+//call Functions
+buildMenu();
+scrollToSection();
+document.addEventListener('scroll', activeStateScrolling);
+
 
 
